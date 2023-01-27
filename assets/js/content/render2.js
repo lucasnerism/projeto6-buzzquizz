184463b8816e9c01c901daffa81e0db6054240e4
Marcos Couto <marcoscouto1997@gmail.com>
2023-01-27 19:53:58 -0300

    tela 2 versao desktop

---
+++ assets/js/content/render2.js
@@ -0,0 +1,75 @@
+import QuizzApiMethods from "../api/quizzApi.js";
+
+let banner = document.querySelector('.banner');
+
+let listaPerguntas = document.querySelector('.listaDePergunta');
+
+let respostas = [];
+
+let quizz = [];
+
+function comparador() { 
+	return Math.random() - 0.5; 
+} 
+
+function buscarQuizz() {
+    const promise = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/2`);
+    promise.then(exibirQuizz);
+    promise.catch('deu errado');
+}
+
+function exibirQuizz(response) {
+    console.log('buscou o quizz')
+    quizz = response.data;
+    TelaquizzEscolhido();
+}
+
+buscarQuizz();
+
+function TelaquizzEscolhido() {
+
+    const template = `
+                <img src="${quizz.image}" alt="imagem do quizz" class="imagemBanner">
+                <p class="tituloQuizz">${quizz.title}</p>
+    `;
+
+    banner.innerHTML += template;
+    
+    for (let i = 0; i < quizz.questions.length; i++) {
+        
+        respostas = [];
+        
+        let lista = `
+            <div class="caixa">
+                <div class="caixaPergunta">
+                    <p class="pergunta">${quizz.questions[i].title}</p>
+                </div> 
+
+                <div class="containerResposta"></div>
+            </div>
+            `;
+
+        listaPerguntas.innerHTML += lista;
+
+        document.querySelector('.caixa:last-child .caixaPergunta').style.backgroundColor = `${quizz.questions[i].color}`
+
+        for (let j = 0; j < quizz.questions[i].answers.length; j++) {
+        
+            let resp = `
+                    <div class="caixaResposta">
+                            <img src="${quizz.questions[i].answers[j].image}" class="imagemOpcao">
+                            <p class="respostaOpcao">${quizz.questions[i].answers[j].text}</p>
+                    </div>  
+                `
+
+            respostas.push(resp);
+            
+        }
+        respostas.sort(comparador);
+
+        for (let k = 0; k < respostas.length; k++) {
+            document.querySelector('.caixa:last-child .containerResposta').innerHTML += respostas[k];
+        }
+    }    
+}
+
